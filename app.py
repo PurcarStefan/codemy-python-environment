c218bc5d4f154027281ca7cca4ec68d5cc5dd4e1
Mihai Bujor <66405930+BujorelActimel@users.noreply.github.com>
2024-09-13 16:39:11 +0000

    Salvare automată la închidere

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 def main():
     print("Salut elevi codemy!")
+    print("Acesta este un program exemplu")
+    print("Pentru a rula programul scrie comanda \'python app.py\' in terminal")
 
 if __name__ == "__main__":
     main()
